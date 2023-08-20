09c0944c428969cf95f420836340dec743521170
lvlrk <lvlrk4u@proton.me>
2023-08-20 17:02:25 -0500

    upkeep

--- cmp/include/lzss.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-typedef unsigned char u8;
-
-int unlzss(unsigned char *src, int srclen, unsigned char *dst, int dstlen);
-int lzss_compress(u8 *in, int insz, u8 *out, int outsz);
--- cmp/include/vcra.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "archive.h"
-
-class Vcra: public Archive {
-public:
-    int ReadFromFile(const std::string& filename);
-    int WriteToFile(const std::string& filename);
-};
